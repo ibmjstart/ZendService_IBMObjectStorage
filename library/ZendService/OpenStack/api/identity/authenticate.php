08214ecc24af9c17713b41fd994527c000eb0148
Alexander Lewitt <alewitt2@gmail.com>
2015-11-17 11:04:32 -0500

    change authentication for OS v3
    change structure of json, change username to userid, add projectid under new scope field.

--- library/ZendService/OpenStack/api/identity/authenticate.php
+++ library/ZendService/OpenStack/api/identity/authenticate.php
@@ -9,16 +9,26 @@
 
 // @see http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_authenticate_v2.0_tokens_Admin_API_Service_Developer_Operations-d1e1356.html
 $result = array(
-    'url' => '/v2.0/tokens',
+    'url' => '/v3/auth/tokens',
     'header' => array(
         'Content-Type' => 'application/json'
     ),
     'method' => 'POST',
     'body' => array(
         'auth' => array(
-            'passwordCredentials' => array(
-                'username' => $params[0],
-                'password' => $params[1]
+            'identity' => array(
+                'methods' => array('password'),
+                'password' => array(
+                    'user' => array(
+                        'id' => $params[0]
+                        'password' => $params[1]
+                    )
+                )
+            ),
+            'scope' => array(
+                'project' => array(
+                    'id' => $params[2]
+                )
             )
         )
     ),
@@ -27,8 +37,8 @@ $result = array(
         'valid_codes' => array('200', '203')
     )
 );
-if (isset($params[2])) {
-    $result['body']['auth']['tenantName'] = $params[2];
-}
+// if (isset($params[3])) {
+//     $result['body']['auth']['tenantName'] = $params[3];
+// }
 $result['body'] = json_encode($result['body']);
 return $result;
